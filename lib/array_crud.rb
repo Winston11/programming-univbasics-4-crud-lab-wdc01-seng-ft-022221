5e9b7bdce85c2ff662349a678a10b2c528de9c7a
Winston McGriff <winstonmcgriff27@gmail.com>
2021-02-10 05:18:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,10 +3,10 @@ def create_an_empty_array
 end
 
 def create_an_array
- questions_array = ["what", "where", "when", "who"]
+ ["what", "where", "when", "who"]
 end
 
-def add_element_to_end_of_array(questions_array, why)
+def add_element_to_end_of_array(array,element)
   
 end
 
@@ -14,7 +14,7 @@ def add_element_to_start_of_array(array, element)
   
 end
 
-def remove_element_from_end_of_array(questions_array)
+def remove_element_from_end_of_array(array)
  
 end
 def remove_element_from_start_of_array(array)
